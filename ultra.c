3a86f722d6d84cb4bb8838c1f8cc8bfcba45b665
Paul Bucci <pbucci@gmail.com>
2013-11-18 14:22:36 -0800

    added ultrasonic fns

---
+++ ultra.c
@@ -0,0 +1,9 @@
+//
+//  ultra.c
+//  l3
+//
+//  Created by bucci on 2013-11-18.
+//  Copyright (c) 2013 bucci. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ ultra.h
@@ -0,0 +1,14 @@
+//
+//  ultra.h
+//  l3
+//
+//  Created by bucci on 2013-11-07.
+//  Copyright (c) 2013 bucci. All rights reserved.
+//
+
+#ifndef l3_ultra_h
+#define l3_ultra_h
+
+
+
+#endif
